be001409c6665c9bcc31c7178fba6f9e2b84b9cd
roy4ik <67188481+roy4ik@users.noreply.github.com>
2020-10-25 15:55:29 +0200

    Accounts model created

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    img = models.ImageField(upload_to = 'media/usr/profile/img/', default='media/usr/defaults/profile/img/default.jpg')
+    job_title = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
