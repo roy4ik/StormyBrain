308a1d507d8286079fe8131f4c9d07be89cc437c
roy4ik <67188481+roy4ik@users.noreply.github.com>
2020-10-26 19:09:56 +0200

    add stormy models to admin closes #27

--- stormy/admin.py
+++ stormy/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Storm)
+admin.site.register(WordRelation)
+admin.site.register(UserWord)
+admin.site.register(Word)
