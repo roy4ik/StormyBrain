adc6db1d94b3a42e01c080cb47bb9acf228bdbe3
roy4ik <67188481+roy4ik@users.noreply.github.com>
2020-10-27 01:08:57 +0200

    created getWordObjects, createSubNodes and searchWord functions - working

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,28 @@
 //stormy getting data from datamuse
 
-getWordObjects = (word, relCode = 'trg', max = 7) => {
+getWordObjects = async(word, relCode = 'trg', max = 7) => {
     apiUrl = 'https://api.datamuse.com/words?rel_' + relCode + '=' + word + '&max=' + max;
-    fetch(apiUrl) // Call API
-        .then((resp) => resp.json()) // Transform the data into json
-        .then(function(data) {
-            console.log(data[0]['word'])
-        })
+    data = []
+    resp = await fetch(apiUrl) // Call API
+    data = await resp.json() // Transform the data into json
         .catch(err => console.log(err))
+    console.log(data)
     return data;
-}
+};
+
+createSubNodes = (data) => {
+    parentNode = document.getElementById('canvas')
+    for (element = 0; data.length - 1; ++element) {
+        console.log('Creating node for :' + data[element]['word'])
+        subNode = document.createElement("div")
+        subNode.innerHTML = data[element]['word']
+        subNode.classList.add = 'subNode'
+        parentNode.appendChild(subNode)
+    };
+};
+
+searchWord = async(wordString = "happy") => {
+    data = await getWordObjects(wordString)
+    createSubNodes(data)
+};
+// todo: save data to user
