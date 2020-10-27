014c630c83feaa1e02b0efc65e1b7c44ebcb04b1
roy4ik <67188481+roy4ik@users.noreply.github.com>
2020-10-27 19:49:18 +0200

    cleaned js closes #34

--- static/js/script.js
+++ static/js/script.js
@@ -32,17 +32,6 @@ searchWord = async(wordString = "happy") => {
     createSubNodes(data)
         // circleSelection(data)
 };
-// todo: save data to user
-
-// todo: calcElementCoord(element)
-
-
-
-// let items = document.querySelectorAll('.circle a');
-// for (var i = 0, l = items.length; i < l; i++) {
-//     items[i].style.left = (50 - 35 * Math.cos(-0.5 * Math.PI - 2 * (1 / l) * i * Math.PI)).toFixed(4) + "%";
-//     items[i].style.top = (50 + 35 * Math.sin(-0.5 * Math.PI - 2 * (1 / l) * i * Math.PI)).toFixed(4) + "%";
-// }
 
 circleSelection = (data) => {
     radius = 200
@@ -59,3 +48,5 @@ circleSelection = (data) => {
     coords = [xValues, yValues]
     return coords
 };
+
+// todo: save data to user
