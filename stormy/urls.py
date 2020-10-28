d59b34fd0dd54e388756ab6326e361d240d8d620
roy4ik <67188481+roy4ik@users.noreply.github.com>
2020-10-28 14:50:13 +0200

    update urls for create save and update api requests

--- stormy/urls.py
+++ stormy/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('stormy', views.addStorm, name="add-storm"),
     path('stormy/<int:storm_pk>', views.stormy, name="stormy"),
     path('stormy/<int:storm_pk>/save-word?word=<slug:word_to_save>&coords=<int:coords_x>&<int:coords_y>', views.saveWord, name='save-word'),
-    path('stormy/<int:storm_pk>/update-userword_rel?initial=<slug:initial_word>&next=<slug:next_word>&rel=<int:rel_score>', views.saveWord, name='save-word'),
+    path('stormy/<int:storm_pk>/update-userword_rel?initial=<slug:initial_word>&next=<slug:next_word>&rel=<int:rel_score>', views.saveWord, name='update-relation'),
 ]
