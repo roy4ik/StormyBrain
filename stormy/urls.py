d999b6491831838d217e28857ff727c13c9b81b6
roy4ik <67188481+roy4ik@users.noreply.github.com>
2020-10-31 12:53:26 +0200

    changed urls to choice_nr

--- stormy/urls.py
+++ stormy/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('stormy', views.addStorm, name="add-storm"),
     path('stormies', views.Stormies.as_view(), name="stormies"),
     path('stormy/<int:storm_pk>', views.stormy, name="stormy"),
-    path('stormy/<int:storm_pk>/save-word/word=<str:word_to_save>/coords=<int:coords_x>&<int:coords_y>', views.saveWord, name='save-word'),
+    path('stormy/<int:storm_pk>/save-word/word=<str:word_to_save>/<int:choice_nr>', views.saveWord, name='save-word'),
     path('stormy/<int:storm_pk>/update-userword_rel/initial=<str:initial_word>&next=<str:next_word>&rel=<int:rel_score>', views.update_userword_relation, name='update-relation'),
 ]
