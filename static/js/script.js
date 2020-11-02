c24763e5dc99383b79667093cb94b5acefe21bb2
roy4ik <67188481+roy4ik@users.noreply.github.com>
2020-11-02 15:43:20 +0200

    completely refactored script  js

--- static/js/script.js
+++ static/js/script.js
@@ -10,107 +10,44 @@ getWordObjects = async(word, relCode = 'trg', max = 7) => {
 
 // creating subnodes for words searched and displaying in semi-circle
 // calculates position of parentElement and adds it to inline style of subnodes
-createSubNodes = (data, parentID = 'search-input') => {
-    canvasNode = document.getElementById('canvas')
-    parentElement = document.getElementById(parentID)
-        // turning new parent from subNode to parentElement
-        // console.log(parentElement.classList)
-    parentElement.classList.remove('subNode')
-    parentElement.classList.add('parentNode')
-
+createSubNodes = (data, parentElement) => {
+    console.log(parentElement)
+        // Determine parent placement
     parentWidth = parentElement.getBoundingClientRect().width
     x = window.scrollX + parentElement.getBoundingClientRect().x
-    console.log(parentElement)
-    remove_non_parentElements()
-    if (parentID == "search-input") {
-        // node for catalyst if parentID = 'search-input')
-        // console.log('Creating node for :' + parentElement.innerHTML)
-        y = window.scrollY + parentElement.getBoundingClientRect().y - 25
-        subNode = document.createElement("div")
-        subNode.innerHTML = parentElement.value
-        subNode.classList.add('parentNode')
-        subNode.classList.add('parentNode-catalyst')
-        subNode.id = parentElement.value
-        subNode.style.left = x + 'px'
-        subNode.style.top = y + 'px'
-        subNode.style.height = 1.5 + 'em';
-        subNode.style.width = parentWidth + 'px'
-        subNode.style.position = 'absolute'
-        subNode.addEventListener('click', function clicked() { searchWord() })
-        canvasNode.appendChild(subNode)
-        parentElement = subNode
-    } else {
-        parentElement.removeEventListener("click", clicked);
-        y = window.scrollY + parentElement.getBoundingClientRect().y
-    }
+    y = window.scrollY + parentElement.getBoundingClientRect().y
+
     console.log(x + ": Left") // position of parentElement
     console.log(y + ": Top")
     coords = circleSelection(data) // get positions for subnodes (top, left)
     nodes = []
     for (element = 0; element < data.length; ++element) {
-        // console.log('parentElement for onclick ' + parentElement.id)
         word = data[element]['word']
             // console.log('Creating node for :' + word)
-        subNode = document.createElement("div")
-        subNode.innerHTML = word
-        subNode.classList.add('subNode')
-        subNode.id = word
-        subNode.style.left = (coords[0][element]) + "px"
-        subNode.style.top = (coords[1][element]) + "px"
-        subNode.style.height = 1.5 + 'em';
+        subnode = create_subNode(word)
+            // add positioning to subnode
+        subNode.style.left = x + (coords[0][element]) + "px"
+        subNode.style.top = y + (coords[1][element]) + "px"
         subNode.style.width = parentWidth + 'px'
-        subNode.style.position = 'relative'
+            // adding dataset
         subNode.dataset.word = word
         subNode.dataset.rel_score = data[element]['score']
-        subNode.addEventListener('click', function clicked() { searchWord(this.dataset.word, this.dataset.rel_score) })
         parentElement.appendChild(subNode)
-        nodes.push(subNode)
+        nodes.push(subNode, coords)
     }
-    remove_parent_events()
-        // return nodes
+    return nodes
 };
 
-function clicked() { searchWord(this.dataset.word, this.dataset.rel_score) }
-
-
-
-// removes all subNodes that are not parentElements too
-remove_non_parentElements = () => {
-    subNodes = document.querySelectorAll('.subNode:not(.parentNode)')
-        // console.log(subNodes)
-    for (node of subNodes.values()) {
-        node.remove()
-    }
-};
-
-remove_parent_events = () => {
-    parents = document.querySelectorAll('.parentNode:not(.search)')
-    for (parent of parents.values()) {
-        parent.removeEventListener("click", clicked);
-    }
-};
-
-searchWord = async(elementID = 'search-input', rel_score = null) => {
-    // console.log("searching word for elementID :" + elementID)
-    if (elementID == 'search-input') {
-        console.log("found input")
-        save_word(document.getElementById('search-input').value)
-        data = await getWordObjects(document.getElementById('search-input').value)
-    } else {
-        data = await getWordObjects(elementID);
-        next_word_node = document.getElementById(elementID);
-        initial = next_word_node.parentElement.id;
-        console.log("initial :" + initial);
-        console.log("next :" + next_word_node.id);
-        // updates the word_relation when selecting a word
-        if (rel_score != null) {
-            update = await update_cloud(initial, next_word_node);
-        };
-    }
-    console.log(data)
-    nodes = createSubNodes(data, parentID = elementID)
-        // return nodes
-};
+create_subNode = (word) => {
+    subNode = document.createElement("div")
+    subNode.innerHTML = subNode.dataset.word
+    subNode.classList.add('subNode')
+    subNode.id = word
+    subNode.style.height = 1.5 + 'em';
+    subNode.style.position = 'relative'
+    subNode.addEventListener('click', function clicked() { searchAndAddWords(subNode) })
+    return subNode
+}
 
 circleSelection = (data) => {
     radius = 200
@@ -126,6 +63,75 @@ circleSelection = (data) => {
     return coords
 };
 
+make_parent = (searchNode) => {
+    // turning new parent from subNode to parentElement
+    searchNode.classList.remove('subNode')
+    searchNode.classList.add('parentNode')
+    searchNode.removeEventListener("click", searchAndAddWords);
+    return searchNode
+}
+
+async function searchAndAddWords(searchNode) {
+    // searchWord(this.dataset.word, this.dataset.rel_score)
+    words = await getWordObjects(searchNode.dataset.word)
+    parentElement = make_parent(searchNode)
+    nodes = createSubNodes(words, parentElement)
+    remove_non_parentElements()
+    remove_parent_events()
+    add_relation(searchNode)
+    console.log("Adding words completed for " + searchNode.dataset.word)
+}
+
+function catalyze() {
+    canvasNode = document.getElementById('canvas')
+    search = document.getElementById('search-input')
+        // console.log('Creating node for :' + parentElement.innerHTML)
+    x = window.scrollX + search.getBoundingClientRect().x
+    y = window.scrollY + search.getBoundingClientRect().y - 25
+    parentWidth = search.getBoundingClientRect().width
+    subnode = create_subNode(search.value)
+    subNode.style.left = x + "px"
+    subNode.style.top = y + "px"
+    subNode.style.width = parentWidth + 'px'
+        // adding data to subnode
+    subNode.dataset.word = search.value
+    save_word(subNode.dataset.word)
+    canvasNode.appendChild(subNode)
+    searchAndAddWords(subNode)
+    return subNode
+}
+
+add_relation = async(searchNode) => {
+    // console.log("searching word for elementID :" + elementID)
+    if (next_word_node.dataset.rel_score != null && initial_word_node != undefined) {
+        next_word_node = searchNode;
+        initial_word_node = next_word_node.parentElement;
+        console.log("initial :" + initial_word_node.dataset.word);
+        console.log("next :" + next_word_node.dataset.word);
+        // updates the word_relation when selecting a word
+        update = await update_cloud(initial, next_word_node);
+        console.log("Rel score updated")
+    };
+};
+
+// removes all subNodes that are not parentElements too
+remove_non_parentElements = () => {
+    subNodes = document.querySelectorAll('.subNode:not(.parentNode)')
+        // console.log(subNodes)
+    for (node of subNodes.values()) {
+        node.remove()
+    }
+};
+
+remove_parent_events = () => {
+    parents = document.querySelectorAll('.parentNode:not(.search)')
+    for (parent of parents.values()) {
+        parent.removeEventListener("click", searchAndAddWords);
+    }
+};
+
+
+
 
 //save data to storm
 // Search word> save initial word
@@ -139,10 +145,10 @@ save_word = async(word_to_save) => {
 };
 
 
-update_cloud = async(initial_word, next_word_node) => {
-    next_word = next_word_node.id
+update_cloud = async(initial_word_node, next_word_node) => {
+    next_word = next_word_node.dataset.word
     rel_score = next_word_node.dataset.rel_score
-    apiUrl = storm + '/update-userword_rel/initial=' + initial_word + '&next=' + next_word + '&rel=' + rel_score;
+    apiUrl = storm + '/update-userword_rel/initial=' + initial_word_node.dataset.word + '&next=' + next_word + '&rel=' + rel_score;
     console.log(apiUrl)
     resp = await fetch(apiUrl)
         .catch(err => console.log(err))
