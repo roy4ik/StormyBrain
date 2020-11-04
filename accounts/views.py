df2c8fae31d1cea588e2609fe2ced3975f21a141
roy4ik <67188481+roy4ik@users.noreply.github.com>
2020-11-04 23:27:38 +0200

    added classes to fields, changed img upload and fixed upload field

--- accounts/views.py
+++ accounts/views.py
@@ -42,6 +42,7 @@ class ProfileUpdate(UpdateView):
     template_name = 'forms/profile-form.html'
     success_url = 'profileDetail'
     failed_message = "The profile couldn't be updated"
+    fields = ['']
 
 
 class ProfileDetail(DetailView):
