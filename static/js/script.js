c6f57878f278ab0d5fb7d0b1b6a1ce7d732cea3f
roy4ik <royaltstaedter@gmail.com>
2021-08-11 21:29:15 +0300

    reverted to no file splitting until move to react

--- static/js/script.js
+++ static/js/script.js
@@ -10,6 +10,17 @@ if (cloud.length > 0) {
     catalyst = cloud[0];
 }
 
+//stormy getting data from datamuse
+let getWordObjects = async(word, relCode = 'trg', max = 7) => {
+    let apiUrl = 'https://api.datamuse.com/words?rel_' + relCode + '=' + word + '&max=' + max
+    let data = []
+    let resp = await fetch(apiUrl) // Call API
+        .catch(err => console.log(err))
+    data = await resp.json() // Transform the data into json
+
+    return data;
+};
+
 // creating subnodes for words searched and displaying in semi-circle
 let createSubNodes = (data, parentElement) => {
     const canvasNode = document.getElementById('canvas')
