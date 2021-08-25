ee4ab7969c5c2bed0b63374c9955e1018086e8dc
roy4ik <royaltstaedter@gmail.com>
2021-08-25 15:09:57 +0300

    fixed __str__ return in profile

--- accounts/models.py
+++ accounts/models.py
@@ -3,22 +3,21 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
+
 # Create your models here.
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
-    img = models.ImageField(upload_to = 'usr/profile/img/', default='usr/defaults/profile/img/default.jpg')
+    img = models.ImageField(upload_to='usr/profile/img/', default='usr/defaults/profile/img/default.jpg')
     job_title = models.CharField(max_length=255)
     company = models.CharField(max_length=255)
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-    
+        return f'{self.user.first_name} {self.user.last_name}'
+
 
 @receiver(post_save, sender=User)
 def create_profile(sender, created, instance, **kwargs):
     if created:
         profile = Profile.objects.create(user=instance)
-
-
